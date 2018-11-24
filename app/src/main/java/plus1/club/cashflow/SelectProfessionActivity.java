125e081dff4ca7aad55c163b898f772ec50fd622
Lavsexpert <lavrov-sergey@yandex.ru>
2018-11-24 23:07:35 +0300

    Профессии вынесены в ресурсы, сделан выбор профессии из списка и настроен внешний вид списка

--- app/src/main/java/plus1/club/cashflow/SelectProfessionActivity.java
+++ app/src/main/java/plus1/club/cashflow/SelectProfessionActivity.java
@@ -3,21 +3,29 @@ package plus1.club.cashflow;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.content.pm.ActivityInfo;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class SelectProfessionActivity extends ListActivity {
-
-    final String[] professions = new String[] { "Автомеханик", "Адвокат", "Водитель грузовика",
-            "Врач", "Дворник", "Инженер", "Медсестра", "Менеджер", "Офицер полиции",
-            "Пилот авиалиний", "Секретарь", "Учитель"};
 
     private ArrayAdapter<String> mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.professions_list);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, professions);
+        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.professions));
         setListAdapter(mAdapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        Toast.makeText(getApplicationContext(),
+                "Вы выбрали " + l.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
     }
 }

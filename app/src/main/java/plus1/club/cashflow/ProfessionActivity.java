1a3f1728c1c419d61b54e907d24242e206195988
Lavsexpert <lavrov-sergey@yandex.ru>
2018-11-25 23:05:32 +0300

    Тест заполнения первых значений в карточке профессии

--- app/src/main/java/plus1/club/cashflow/ProfessionActivity.java
+++ app/src/main/java/plus1/club/cashflow/ProfessionActivity.java
@@ -5,11 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class ProfessionActivity extends AppCompatActivity {
 
     public int profession;
+    public String[] numbers;
+    public int[] arrays =
+            {R.array.profession1, R.array.profession2, R.array.profession3, R.array.profession4,
+            R.array.profession5, R.array.profession6, R.array.profession7, R.array.profession8,
+            R.array.profession9, R.array.profession10, R.array.profession11, R.array.profession12};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +26,24 @@ public class ProfessionActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String message = intent.getStringExtra(SelectProfessionActivity.EXTRA_MESSAGE);
         profession = Integer.parseInt(message);
-        TextView editText = (TextView) findViewById(R.id.textProfession);
+        TextView editProfession = (TextView) findViewById(R.id.textProfession);
 
         // Если выбран пункт "Случайная профессия" - выбираем случайную профессию из списка
         if (profession == 0){
             Random rand = new Random();
             profession = rand.nextInt(12) + 1;
         }
-        editText.setText(getResources().getStringArray(R.array.professions)[profession]);
+        editProfession.setText(getResources().getStringArray(R.array.professions)[profession]);
+
+        // Поиск заполняемых полей
+        TextView editSalary = (TextView) findViewById(R.id.textSalary);
+        TextView editFullRevenue = (TextView) findViewById(R.id.textFullRevenue);
+
+        // Получение знанчений для заполнения
+        numbers = getResources().getStringArray(arrays[profession-1]).clone();
+
+        // Заполнение карточки профессии
+        editSalary.setText(numbers[0]);
+        editFullRevenue.setText(numbers[1]);
     }
 }
